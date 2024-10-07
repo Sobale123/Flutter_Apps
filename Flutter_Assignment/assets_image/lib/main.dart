88753990b1151c8fe6d4267f9fc9306058ae644b
Sobale123 <129029038+Sobale123@users.noreply.github.com>
2024-10-07 16:34:47 +0530

    Asset

---
+++ Flutter_Assignment/assets_image/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title:const Text("Image Demo"),
+        ),
+        body: Center(
+          child: Image.asset("assets/asset.jpeg"),
+        ),
+      ),
+    );
+  }
+}
